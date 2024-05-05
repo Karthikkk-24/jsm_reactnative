9adbf998176c84985c7ba8a732c324a6b14c4394
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-05-05 11:50:45 +0530

    Imported External Fonts

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Karthik Shettigar</Text>
+      <Text className="text-3xl font-pblack">Karthik Shettigar</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue' }}>Go To Profile</Link>
     </View>
