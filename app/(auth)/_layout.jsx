9723bf1bc6969817419749eb9cffcf19dcc6504c
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-05-11 10:14:09 +0530

    Sign In Changes

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
-  )
-}
+    <>
+      <Stack>
+        <Stack.Screen
+          name="sign-in"
+          options={{
+            headerShown: false,
+          }}
+        />
+        
+        <Stack.Screen
+          name="sign-up"
+          options={{
+            headerShown: false,
+          }}
+        />
+        
+        <StatusBar style="light" backgroundColor="#161622" />
+      </Stack>
+    </>
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../../constants";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+           
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default SignIn
+export default SignIn;
