f0689e02533c6f081f6fe7d1af8811df08cac49a
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-05-15 22:34:19 +0530

    Added changes to custom form field

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from "react";
+import { Text, TextInput, View } from "react-native";
 
-const FormField = () => {
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+
+    const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <View className="space-y-2">
-      <Text className="text-base text-gray-100 font-pmedium">FormField</Text>
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <View className="w-full h-12 px-4 bg-black-100 border-2 border-black-200 rounded-xl focus:border-secondary items-center">
+        <TextInput
+          className="flex-1 text-white font-psemibold text-base"
+          value={value}
+          placeholder={placeholder}
+          onChangeText={handleChangeText}
+          placeholderTextColor="#7b7b8b"
+          secureTextEntry={title === "Password" && !showPassword}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default FormField
+export default FormField;
