c981f12c4d0e5b3a52a0c882427785a1683b6402
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-05-26 11:10:48 +0530

    Added Global Provider to the main route

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,6 +3,8 @@ import { Slot, SplashScreen, Stack } from "expo-router";
 import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import GlobalProvider from "../context/GlobalProvider";
+
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
@@ -25,21 +27,23 @@ const RootLayout = () => {
     }
 
     if (fontsLoaded) {
-        SplashScreen.hideAsync();
+      SplashScreen.hideAsync();
     }
 
     if (!fontsLoaded && !error) {
-        return null;
+      return null;
     }
   }, [fontsLoaded, error]);
 
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
+      </Stack>
+    </GlobalProvider>
   );
 };
 
